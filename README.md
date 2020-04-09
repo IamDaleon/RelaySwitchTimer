c9d57ef6d7e22ab0fc9955488cf97a73ff3a9e01
Daleon L <daleon86@gmail.com>
2020-04-08 19:35:03 -0500

    🔥🔥🔥 Update 🔥🔥🔥

---
+++ README.md
@@ -0,0 +1,56 @@
+# RelaySwitchTimer
+
+This little device allows you to set a time ⏲ to when to turn on and off the device, that it is connected too. Leaving a light on constantly on is a huge waste of electricity 🔌, so let add a microcontroller to make it a little pockect saver on our pockets.
+
+## Before getting started
+
+⚠️️️️⚠️⚠️⚠️️️️⚠️⚠️⚠️️️️⚠️⚠️⚠️️️️⚠️⚠️
+
+>Warning: Always be very careful when experimenting with AC, electrical shock can result in serious injures/Death.
+
+>Relay module from bottom side is open when AC is connected do not touch the circuit.
+
+⚠️️️️⚠️⚠️⚠️️️️⚠️⚠️⚠️️️️⚠️⚠️⚠️️️️⚠️⚠️
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+Here are some things you'd need to get this project going.
+
+```
+ Arduino
+ Velleman Data Logging Sheild
+ Evil Mad Scientist Simple Relay Switch
+ USB Cable
+ Dollar Store Extenstion cord
+
+```
+
+## Built With
+
+* [Arduino v1.8.10](http://www.arduino.cc) - The IDE used.
+* [PlatformIO v4.3.1](http://www.platformio.org) - Code & upload to the Arduino within VSCode.
+
+## Contributing
+
+Please read [CONTRIBUTING to the project](https://gist.github.com/IamDaleon/ee0a3a6a7b3d9bad014ccdae77d39c83) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+## Authors
+
+* **Daleon Listhrop** - *Initial work* - [iamDaleon](https://github.com/iamDaleon)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* [ Inland ](https://www.microcenter.com/product/486544/inland-uno-r3-mainboard)
+* [ Velleman ](https://www.velleman.eu/)
+* [ Evil Mad Scientist ](https://shop.evilmadscientist.com/)
+* [ USB Type B ](https://www.microcenter.com/product/202296/qvs-usb-20-(type-a)-male-to-usb-20-(type-b)-male-cable-1-ft---beige)
+* [ Dollar General ](https://www.dollargeneral.com/dg-hardware-extension-cord-9-assorted-colors.html/)
---
+++ sketch_or-code/AwayLight/AwayLight.ino
@@ -0,0 +1,133 @@
+#include <Arduino.h>
+#include <Wire.h> // must be included here so that Arduino library object file references work
+#include <RtcDS1307.h>
+
+RtcDS1307<TwoWire> Rtc(Wire);
+
+const int Relay = 4;
+const int delayPeriod = 50;
+
+void setup ()
+{
+  Serial.begin(115200);
+
+  Serial.print("Loadout Date Acquired: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+  if (!Rtc.IsDateTimeValid())
+  {
+    if (Rtc.LastError() != 0)
+    { //https://www.arduino.cc/en/Reference/WireEndTransmission
+      Serial.print("Onboard time has encounted communications error = ");
+      Serial.println(Rtc.LastError());
+    }
+    else
+    { //    First Boot / Device not ready. Battery is low or missing
+      Serial.println("Onoard time lost confidence in the DateTime!");
+      Rtc.SetDateTime(compiled);
+    }
+  }
+
+  if (!Rtc.GetIsRunning())
+  {
+    Serial.println("Onboard time was not actively running, Initalizing now");
+    Rtc.SetIsRunning(true);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (RtcDateTime(now) < compiled)
+  {
+    Serial.println("Onboard time experienced a delay! (Updating now)");
+    Rtc.SetDateTime(compiled);
+  }
+  else if (now > compiled)
+  {
+    Serial.println("Onboard time is ahead System time. (Resequencing now)");
+  }
+  else if (now == compiled)
+  {
+    Serial.println("Onboard time clear to initialization ( All is Well!)");
+  }
+
+  // never assume the Rtc was last configured by you, so
+  // just clear them to your needed state
+  Rtc.SetSquareWavePin(DS1307SquareWaveOut_Low);
+
+  // Switch/ Relay
+  pinMode(Relay, OUTPUT);
+}
+
+void loop ()
+{
+  if (!Rtc.IsDateTimeValid())
+  {
+    if (Rtc.LastError() != 0)
+    {
+      Serial.print("Onboard time communications error = ");
+      Serial.println(Rtc.LastError());
+    }
+    else
+    {
+      Serial.println("Onboard time lost confidence in the DateTime!"); // The battery is low or even missing / power was disconnected
+    }
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+  Serial.println();
+
+  delay(10000); // ten seconds
+
+  const int printHour = now.Hour();
+
+  if (printHour >= 18)
+  {
+    digitalWrite(Relay, HIGH);
+  } else if (printHour <= 7) {
+    digitalWrite(Relay, LOW);
+  }
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+  char datestring[20];
+
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+             dt.Month(),
+             dt.Day(),
+             dt.Year(),
+             dt.Hour(),
+             dt.Minute(),
+             dt.Second() );
+  Serial.print(datestring);
+}
+void printDate(const RtcDateTime& dt)
+{
+  char datestring[3];
+
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u"),
+             dt.Day());
+  Serial.print(datestring);
+}
+void printHour(const RtcDateTime& dt)
+{
+  char datestring[3];
+
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u"),
+             dt.Hour());
+  Serial.print(datestring);
+}
